60f549c3f60172d5b08a638d52d655ac81979fc6
Lud0110 <pumalud0110@gmai.com>
2022-06-30 13:00:27 -0500

    commit 2

--- funciones.py
+++ funciones.py
@@ -8,18 +8,26 @@
 # imprimir_mensaje()
 
 
-def conversacion(mensaje):
-    print('hola')
-    print('Como estas')
-    print(mensaje)
-    print('Adios')
+# def conversacion(mensaje):
+#     print('hola')
+#     print('Como estas')
+#     print(mensaje)
+#     print('Adios')
 
-opcion = int(input('Elige una opcion (1, 2, 3): '))
-if opcion == 1:
-    conversacion('Eligiste la opcion 1')   
-elif opcion == 2:
-    conversacion('Eligiste la opcion 2')       
-elif opcion == 3:
-    conversacion('Eligiste la opcion 3')    
-else:
-    print('Escribe la opcion correcta')          
+# opcion = int(input('Elige una opcion (1, 2, 3): '))
+# if opcion == 1:
+#     conversacion('Eligiste la opcion 1')   
+# elif opcion == 2:
+#     conversacion('Eligiste la opcion 2')       
+# elif opcion == 3:
+#     conversacion('Eligiste la opcion 3')    
+# else:
+#     print('Escribe la opcion correcta')          
+
+def suma(a, b):
+    print('Se suman dos numeros')
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4) 
+print(sumatoria)  
